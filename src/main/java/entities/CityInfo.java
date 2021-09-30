15cc79968dad17cf13f1f757562f4b41e0dfc632
MagnusGranno <70953393+MagnusGranno@users.noreply.github.com>
2021-09-30 14:13:37 +0200

    entities 2.0

--- src/main/java/entities/CityInfo.java
+++ src/main/java/entities/CityInfo.java
@@ -31,25 +31,7 @@ public class CityInfo implements Serializable {
         this.id = id;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof CityInfo)) {
-            return false;
-        }
-        CityInfo other = (CityInfo) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
+    
 
     @Override
     public String toString() {
