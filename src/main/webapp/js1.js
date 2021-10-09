2e72feaface6dd4cb8c47adc4eccd441086f5751
LukasCph <cph-lo140@cphbusiness.dk>
2021-10-09 20:51:27 +0200

    commit changes to js1/index

--- src/main/webapp/js1.js
+++ src/main/webapp/js1.js
@@ -1,29 +1,107 @@
+
+
 function getPersonsByHobby() {
-fetch("http://localhost:8080/dat3-startcode/api/person/hobby/bage")
-  .then(function(response) {
-    return response.json();
-   })
-    .then(function(data) {
-    console.log(data);
-    let obj = JSON.parse(data);
+    fetch('http://localhost:8080/dat3-startcode/api/person/hobby/bage').then(function(response){
+        return response.json();
+    }).then(data => {
+    let obj = data;
+       var table = document.getElementById("persontablebody");    
+       while(table.hasChildNodes()){
+           table.removeChild(table.firstChild);
+       }
+         console.log(obj[0]);
+         console.log(obj[1]);
+       
+    for(let i=0;i < obj.length; i++){
+
+    var row = table.insertRow(i);
     
-    createTable();
-    console.log(obj);
-    cell1.innerHTML = obj[0];
-   });
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    var cell3 = row.insertCell(3);
+    var cell4 = row.insertCell(4);
+    var cell5 = row.insertCell(5);
+    var cell6 = row.insertCell(6);
+ 
+    cell0.innerHTML = obj[i].id;
+    cell1.innerHTML = obj[i].email;
+    cell2.innerHTML = obj[i].firstName;
+    cell3.innerHTML = obj[i].lastName;
+    cell4.innerHTML = "Hobby goes here";
+    cell5.innerHTML = "Phone goes here";
+    cell6.innerHTML = "Street/Zip/City";
+        }
+    });
 }
 
-function createTable(){
-var table = document.getElementById("persontable");
-var row = table.insertRow(1);
-var row1 = table.insertRow(2);
+function getPersonsByPhone() {
+    fetch('http://localhost:8080/dat3-startcode/api/person/phone/23756493').then(function(response){
+        return response.json();
+    }).then(data => {
+    let obj = data;
+       var table = document.getElementById("persontablebody");    
+       while(table.hasChildNodes()){
+           table.removeChild(table.firstChild);
+       }
+         console.log(obj);
 
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-
-
-cell2.innerHTML = "NEW CELL2";
+    var row = table.insertRow(0);
+    
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    var cell3 = row.insertCell(3);
+    var cell4 = row.insertCell(4);
+    var cell5 = row.insertCell(5);
+    var cell6 = row.insertCell(6);
+ 
+    cell0.innerHTML = obj.id;
+    cell1.innerHTML = obj.email;
+    cell2.innerHTML = obj.firstName;
+    cell3.innerHTML = obj.lastName;
+    cell4.innerHTML = "Hobby goes here";
+    cell5.innerHTML = "Phone goes here";
+    cell6.innerHTML = "Street/Zip/City";
+        
+    });
 }
+
+function getPersonsByCity() {
+    fetch('http://localhost:8080/dat3-startcode/api/person/city/københavn').then(function(response){
+        return response.json();
+    }).then(data => {
+    let obj = data;
+       var table = document.getElementById("persontablebody");    
+       while(table.hasChildNodes()){
+           table.removeChild(table.firstChild);
+       }
+         console.log(obj[0]);
+         console.log(obj[1]);
+       
+    for(let i=0;i < obj.length; i++){
+
+    var row = table.insertRow(i);
+    
+    var cell0 = row.insertCell(0);
+    var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
+    var cell3 = row.insertCell(3);
+    var cell4 = row.insertCell(4);
+    var cell5 = row.insertCell(5);
+    var cell6 = row.insertCell(6);
+ 
+    cell0.innerHTML = obj[i].id;
+    cell1.innerHTML = obj[i].email;
+    cell2.innerHTML = obj[i].firstName;
+    cell3.innerHTML = obj[i].lastName;
+    cell4.innerHTML = "Hobby goes here";
+    cell5.innerHTML = "Phone goes here";
+    cell6.innerHTML = "Street/Zip/City";
+        }
+    });
+}
+
 
 //
 //let options = {
